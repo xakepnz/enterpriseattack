e11c7af3a172ef589fea9301f2d0f97e509bbc76
xakepnz <xakepnz@pm.me>
2024-04-06 00:27:45 +1300

    add non subscript test

---
+++ tests/non_subscriptable.py
@@ -0,0 +1,23 @@
+# ----------------------------------------------------------------------------#
+
+import enterpriseattack
+
+# ----------------------------------------------------------------------------#
+
+
+def test_non_subscriptable():
+    attack = enterpriseattack.Attack(
+        mitre_version='latest',
+        subscriptable=False,
+        update=True,
+        include_deprecated=True
+    )
+
+    assert 'Lazarus Group' in [obj.name for obj in attack.groups]
+    assert 'Resource Development' in [obj.name for obj in attack.tactics]
+    assert 'Process Injection' in [obj.name for obj in attack.techniques]
+    assert 'SSH Hijacking' in [obj.name for obj in attack.sub_techniques]
+    assert 'Network Traffic' in [obj.name for obj in attack.data_sources]
+    assert 'Code Signing' in [obj.name for obj in attack.mitigations]
+    assert 'Cobalt Strike' in [obj.name for obj in attack.software]
+    assert 'Operation Sharpshooter' in [obj.name for obj in attack.campaigns]
