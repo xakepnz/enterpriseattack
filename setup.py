f8f27b3076e66563b1f6eb612a62b670b82c348f
xakepnz <xakepnz@pm.me>
2024-04-14 22:31:11 +1200

    add dynamic reqs & update py versions

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+# ----------------------------------------------------------------------------#
+
 import os
 import re
 from setuptools import setup, find_packages
 from io import open
+
+# ----------------------------------------------------------------------------#
 
 with open(
     os.path.join(
@@ -11,8 +15,17 @@ with open(
         )) as fp:
     VERSION = re.match(r'.*__version__ = \'(.*?)\'', fp.read(), re.S).group(1)
 
+# ----------------------------------------------------------------------------#
+
 with open('README.md', 'r', encoding='utf-8') as fp:
-    readme = fp.read()
+    README = fp.read()
+
+# ----------------------------------------------------------------------------#
+
+with open('requirements.txt', 'r', encoding='utf-8') as fp:
+    REQUIREMENTS = fp.read().splitlines()
+
+# ----------------------------------------------------------------------------#
 
 setup(
     name='enterpriseattack',
@@ -21,8 +34,11 @@ setup(
     version=VERSION,
     packages=find_packages(exclude=['tests*', 'docs*']),
     description='A lightweight Python module to interact with the '
-                'Mitre Att&ck Enterprise framework.',
-    long_description=readme,
+                '[MITRE ATT&CK®](https://attack.mitre.org/) Enterprise '
+                'dataset. Built for speed with minimal dependencies. '
+                '[Read the docs](https://gitlab.com/xakepnz/enterpriseattack/tree/main/docs) '  # noqa: E501
+                'for more info.',
+    long_description=README,
     long_description_content_type='text/markdown',
     url='https://gitlab.com/xakepnz/enterpriseattack',
     keywords=[
@@ -30,13 +46,11 @@ setup(
         'att&ck enterprise',
         'enterpriseattack',
         'mitre framework',
-        'att&ck'
+        'att&ck',
+        'python MITRE ATT&CK'
     ],
     include_package_data=True,
-    install_requires=[
-        'ujson >= 3.0.0',
-        'requests >= 2.9.2'
-    ],
+    install_requires=REQUIREMENTS,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -49,6 +63,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
