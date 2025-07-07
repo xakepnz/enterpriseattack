840054c1776c77a62a9937c8977c677447b20c95
xakepnz <xakepnz@pm.me>
2025-07-08 11:54:54 +1200

    fix: Refresh semantic-release token, hardcode version for auto update

--- enterpriseattack/__init__.py
+++ enterpriseattack/__init__.py
@@ -8,7 +8,7 @@ from enterpriseattack import (campaign, component, data_source, group,
 
 # ----------------------------------------------------------------------------#
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 # ----------------------------------------------------------------------------#
 # enterpriseattack Attack class:
